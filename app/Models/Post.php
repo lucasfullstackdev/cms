250240908f40d045850ed3dc2e7f3ba6bcae2910
lucas oliveira <lucas.fullstack.dev@gmail.com>
2024-06-07 21:01:54 -0300

    feat: Abstracting common features of models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
@@ -21,6 +21,13 @@ class Post extends Model
         'created_by',
         'updated_by',
     ];
+
+    public function publishAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->formatDateTime($value),
+        );
+    }
 
     public function status()
     {
