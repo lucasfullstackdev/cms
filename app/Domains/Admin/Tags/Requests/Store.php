f81d279542fceff950ea1658f1dd54dba81d65e2
lucas oliveira <lucas.fullstack.dev@gmail.com>
2024-06-11 10:14:16 -0300

    feat: Add validation for tag creation

---
+++ app/Domains/Admin/Tags/Requests/Store.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Domains\Admin\Tags\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class Store extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::user() && Auth::user()->isAdmin();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255|unique:tags,name',
+            'active' => 'required|boolean',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'errors'  => $validator->errors(),
+            'data'    => []
+        ], Response::HTTP_BAD_REQUEST));
+    }
+}
