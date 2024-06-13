eac5cb4487f28212b6be61a398afb3191e518eb0
lucas oliveira <lucas.fullstack.dev@gmail.com>
2024-06-13 08:17:59 -0300

    refactor: Remove null properties to return a smaller payload

--- app/Dtos/Dto.php
+++ app/Dtos/Dto.php
@@ -2,10 +2,27 @@
 
 namespace App\Dtos;
 
+use Illuminate\Support\Collection;
+
 abstract class Dto
 {
   public final function toArray(): array
   {
-    return get_object_vars($this);
+    return array_filter(get_object_vars($this), fn ($value) => !is_null($value));
+  }
+
+  public final function toResponse(): array
+  {
+    return array_map(function ($value) {
+      if ($value instanceof Dto) {
+        return $value->toArray();
+      }
+
+      if ($value instanceof Collection) {
+        return $value->map(fn ($item) => $item->toArray());
+      }
+
+      return $value;
+    }, $this->toArray());
   }
 }
